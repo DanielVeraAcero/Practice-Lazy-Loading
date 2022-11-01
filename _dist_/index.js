ef98ade2f243a1eb6770fccf719402c38d23b113
DanielVeraAcero <DanielVeraAcero@users.noreply.github.com>
2022-11-01 03:40:39 +0000

    deploy: 1235d634f367ae5ebd327409532f97c97682c543

---
+++ _dist_/index.js
@@ -0,0 +1,38 @@
+import { registerImage } from "./lazy-loading.js";
+
+/* 
+<div class="p-4">
+    <img class="mx-auto" width="320" src="https://randomfox.ca/images/2.jpg" alt="">
+</div>
+*/
+const addButton = document.querySelector('button')
+const mountNode = document.querySelector('#images')
+
+const foxCount = 123;
+const randomFoxNumber = () => {
+    const randomNumber = Math.ceil(Math.random()*(foxCount));
+    console.log(randomNumber);
+    return randomNumber
+}
+
+const createImgNode = () => {
+    const container = document.createElement('div');
+    container.classList.add('p-4');
+
+    const image = document.createElement('img');
+    image.classList.add('mx-auto');
+    image.width = 320;
+    image.dataset.src = `https://randomfox.ca/images/${randomFoxNumber()}.jpg`;
+
+    container.appendChild(image);
+
+    return container
+};
+
+const getFox = () => {
+    const newImage = createImgNode();
+    mountNode.appendChild(newImage);
+    registerImage(newImage);
+}
+
+addButton.addEventListener('click', getFox)
